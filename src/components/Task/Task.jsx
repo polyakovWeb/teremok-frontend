960495a3849985c73130a1b9072369b10f70aaf7
PakostTosha <polyakov.a.a.03@mail.ru>
2025-06-16 17:02:20 +0700

    Update distraction, delete handclap timer

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import TaskInput from "../TaskInput/TaskInput";
-import appleImg from "../../img/apple.svg";
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import TaskInput from '../TaskInput/TaskInput';
+import appleImg from '../../img/apple.svg';
 
 function Task({
 	mathExpressions,
@@ -23,7 +23,7 @@ function Task({
 	const [tryCount, setTryCount] = useState(0);
 
 	const { register, handleSubmit, reset } = useForm({
-		mode: "onSubmit",
+		mode: 'onSubmit',
 		defaultValues: currentExpression,
 	});
 
@@ -37,19 +37,19 @@ function Task({
 	let operand_1;
 	const selectedTypeTask = mathExpressions.type;
 	switch (selectedTypeTask) {
-		case "Сложение":
-			operand_1 = "+";
+		case 'Сложение':
+			operand_1 = '+';
 			taskInfo = `Впиши недостающую цифру вместо "?" так, чтобы  результат сложения слева был равен цифре справа`;
 			break;
-		case "Вычитание":
-			operand_1 = "-";
+		case 'Вычитание':
+			operand_1 = '-';
 			taskInfo = `Впиши недостающую цифру вместо "?" так, чтобы  результат вычитания слева был равен цифре справа`;
 			break;
-		case "Порядковый счет":
-			taskInfo = "Описание действия в задачах на порядковый счёт";
+		case 'Порядковый счет':
+			taskInfo = 'Описание действия в задачах на порядковый счёт';
 			break;
-		case "Состав числа":
-			taskInfo = "Описание действия в задачах на состав числа";
+		case 'Состав числа':
+			taskInfo = 'Описание действия в задачах на состав числа';
 			break;
 
 		default:
@@ -66,15 +66,15 @@ function Task({
 		// Блок проверки правильности результата
 		let userResult, isEqual;
 		switch (selectedTypeTask) {
-			case "Сложение":
+			case 'Сложение':
 				userResult = firstNum + secondNum;
 				isEqual = userResult === result;
 				break;
-			case "Вычитание":
+			case 'Вычитание':
 				userResult = firstNum - secondNum;
 				isEqual = userResult === result;
 				break;
-			case "Порядковый счет":
+			case 'Порядковый счет':
 				// Дано: [1,6,8,2,5] / "16825"
 				// Программно сортировать строку
 				// Полученный образец: [1,2,5,6,8] / "12568"
@@ -82,7 +82,7 @@ function Task({
 				// Получено 1: [1,2,6,5,8] / "12658" === образ."12568" - false
 				// Получено 2: [1,2,5,6,8] / "12568" === образ."12568" - true
 				break;
-			case "Состав числа":
+			case 'Состав числа':
 				// В чём заключается смысл задания?
 				break;
 			default:
@@ -94,7 +94,7 @@ function Task({
 			// Если правильно решено с 1-ой попытки, то - награда + следующее задание
 			if (tryCount === 0) {
 				setIteration(iteration + 1);
-				setActiveComponent("CardGreeting");
+				setActiveComponent('CardGreeting');
 				if (iteration !== mathExpressions.variants.length - 1) {
 					// Переход к следующей задаче (обновление данных задачи)
 					setCurrentExpression(mathExpressions.variants[iteration + 1]);
@@ -102,7 +102,7 @@ function Task({
 			} else {
 				setTryCount(0);
 				reset();
-				setActiveComponent("Timer");
+				setActiveComponent('Distraction');
 			}
 		} else {
 			reset();
@@ -127,48 +127,51 @@ function Task({
 			.map((_, index) => (
 				<img
 					src={appleImg}
-					alt="Подсказка"
-					className="helpImg"
+					alt='Подсказка'
+					className='helpImg'
 					key={index}
-					width="45px"
+					width='45px'
 				/>
 			));
 	};
 
 	// Корректное отображение слова "яблоко"
 	const correctWord = (count) => {
-		if (count === 1) return "яблоко";
-		if (count > 1 && count < 5) return "яблока";
-		if (count === 0 || (count > 4 && count < 10)) return "яблок";
+		if (count === 1) return 'яблоко';
+		if (count > 1 && count < 5) return 'яблока';
+		if (count === 0 || (count > 4 && count < 10)) return 'яблок';
 	};
 
 	return (
-		<div className="task">
+		<div className='task'>
 			{tryCount > 0 ? (
 				<>
-					<h1 className="title">Ой, что-то не так!</h1>
-					<p className="info">
-						Была допущена ошибка. Посмотри на подсказку и попробуй выполнить задание
-						снова
+					<h1 className='title'>Ой, что-то не так!</h1>
+					<p className='info'>
+						Была допущена ошибка. Посмотри на подсказку и попробуй
+						выполнить задание снова
 					</p>
 				</>
 			) : (
 				<>
-					<h1 className="title">{selectedTypeTask}</h1>
-					<p className="info">{taskInfo}</p>
+					<h1 className='title'>{selectedTypeTask}</h1>
+					<p className='info'>{taskInfo}</p>
 				</>
 			)}
-			<div className="task-wrapper">
-				<form className="task-container" onSubmit={handleSubmit(checkingTask)}>
+			<div className='task-wrapper'>
+				<form
+					className='task-container'
+					onSubmit={handleSubmit(checkingTask)}
+				>
 					{/* Блок подсказок */}
 					{tryCount > 0 && (
 						<>
-							<div className="help">
-								<div className="first-number helper">
-									<div className="imgs-container">
+							<div className='help'>
+								<div className='first-number helper'>
+									<div className='imgs-container'>
 										{addAppleImgs(currentExpression.help[0])}
 									</div>
-									<p className="imgs-count">
+									<p className='imgs-count'>
 										{tryCount > 1 &&
 											`${currentExpression.help[0]} ${correctWord(
 												currentExpression.help[0]
@@ -176,24 +179,24 @@ function Task({
 									</p>
 								</div>
 
-								<div className="operation operand-1">{operand_1}</div>
-								<div className="second-number helper">
-									<div className="imgs-container">
+								<div className='operation operand-1'>{operand_1}</div>
+								<div className='second-number helper'>
+									<div className='imgs-container'>
 										{addAppleImgs(currentExpression.help[1])}
 									</div>
-									<p className="imgs-count">
+									<p className='imgs-count'>
 										{tryCount > 1 &&
 											`${currentExpression.help[1]} ${correctWord(
 												currentExpression.help[1]
 											)}`}
 									</p>
 								</div>
-								<div className="operation operand-2">=</div>
-								<div className="result helper">
-									<div className="imgs-container">
+								<div className='operation operand-2'>=</div>
+								<div className='result helper'>
+									<div className='imgs-container'>
 										{addAppleImgs(currentExpression.help[2])}
 									</div>
-									<p className="imgs-count">
+									<p className='imgs-count'>
 										{tryCount > 1 &&
 											`${currentExpression.help[2]} ${correctWord(
 												currentExpression.help[2]
@@ -204,39 +207,40 @@ function Task({
 						</>
 					)}
 
-					<div className="task-content">
-						{selectedTypeTask === "Сложение" || selectedTypeTask === "Вычитание" ? (
+					<div className='task-content'>
+						{selectedTypeTask === 'Сложение' ||
+						selectedTypeTask === 'Вычитание' ? (
 							<>
 								{/* Разметка для вычитания/сложения */}
 								{/* 1 число */}
 								<TaskInput
-									className="number number-1"
-									name="firstNum"
-									type="number"
+									className='number number-1'
+									name='firstNum'
+									type='number'
 									register={register}
 									min={0}
 									max={9}
 									curTask={currentExpression}
 								/>
 								{/* знак + или - */}
-								<div className="operation operand-1">{operand_1}</div>
+								<div className='operation operand-1'>{operand_1}</div>
 								{/* 2 число */}
 								<TaskInput
-									className="number number-2"
-									name="secondNum"
-									type="number"
+									className='number number-2'
+									name='secondNum'
+									type='number'
 									register={register}
 									min={0}
 									max={9}
 									curTask={currentExpression}
 								/>
 								{/* знак = */}
-								<div className="operation operand-2">=</div>
+								<div className='operation operand-2'>=</div>
 								{/* 3 число - результат действия */}
 								<TaskInput
-									className="number number-3"
-									name="result"
-									type="number"
+									className='number number-3'
+									name='result'
+									type='number'
 									register={register}
 									min={0}
 									max={9}
@@ -245,15 +249,17 @@ function Task({
 							</>
 						) : (
 							<>
-								<div className="">"Разметка состав числа/порядковый счёт"</div>
+								<div className=''>
+									"Разметка состав числа/порядковый счёт"
+								</div>
 							</>
 						)}
 					</div>
-					<div className="task-footer">
-						<button type="submit" className="button-common button">
+					<div className='task-footer'>
+						<button type='submit' className='button-common button'>
 							Проверить
 						</button>
-						<p className="task-number">Задание №{iteration + 1}</p>
+						<p className='task-number'>Задание №{iteration + 1}</p>
 					</div>
 				</form>
 			</div>
--- src/pages/Trainer/Trainer.jsx
+++ src/pages/Trainer/Trainer.jsx
@@ -1,32 +1,32 @@
-import "./Trainer.scss";
-import { useAuth } from "../../components/AuthContext/AuthContext.js";
-import { useState } from "react";
-import TrainerInit from "../../components/TrainerInit/TrainerInit.jsx";
-import TrainerTask from "../../components/TrainerTask/TrainerTask.jsx";
-import CardSelection from "../../components/CardSelection/CardSelection.jsx";
-import card1 from "../../img/card1.jpg";
-import card2 from "../../img/card2.jpg";
-import card3 from "../../img/card3.jpg";
-import card4 from "../../img/card4.jpg";
-import card5 from "../../img/card5.jpg";
-import Task from "../../components/Task/Task.jsx";
-import CardGreeting from "../../components/CardGreeting/CardGreeting.jsx";
-import Cartoon from "../../components/Cartoon/Cartoon.jsx";
-import HandClapTimer from "../../components/HandClapTimer/HandClapTimer.jsx";
+import './Trainer.scss';
+import { useAuth } from '../../components/AuthContext/AuthContext.js';
+import { useState } from 'react';
+import TrainerInit from '../../components/TrainerInit/TrainerInit.jsx';
+import TrainerTask from '../../components/TrainerTask/TrainerTask.jsx';
+import CardSelection from '../../components/CardSelection/CardSelection.jsx';
+import card1 from '../../img/card1.jpg';
+import card2 from '../../img/card2.jpg';
+import card3 from '../../img/card3.jpg';
+import card4 from '../../img/card4.jpg';
+import card5 from '../../img/card5.jpg';
+import Task from '../../components/Task/Task.jsx';
+import CardGreeting from '../../components/CardGreeting/CardGreeting.jsx';
+import Cartoon from '../../components/Cartoon/Cartoon.jsx';
+import Distraction from '../../components/Distraction/Distraction.jsx';
 
 // Массив карточек на выбор
 const cardsImgs = [
-	{ id: 1, src: card1, alt: "Card 1" },
-	{ id: 2, src: card2, alt: "Card 2" },
-	{ id: 3, src: card3, alt: "Card 3" },
-	{ id: 4, src: card4, alt: "Card 4" },
-	{ id: 5, src: card5, alt: "Card 5" },
+	{ id: 1, src: card1, alt: 'Card 1' },
+	{ id: 2, src: card2, alt: 'Card 2' },
+	{ id: 3, src: card3, alt: 'Card 3' },
+	{ id: 4, src: card4, alt: 'Card 4' },
+	{ id: 5, src: card5, alt: 'Card 5' },
 ];
 
 // Массив заданий
 const tasksList = [
 	{
-		type: "Сложение",
+		type: 'Сложение',
 		variants: [
 			{ firstNum: 1, secondNum: null, result: 4, help: [1, 3, 4] },
 			{ firstNum: 2, secondNum: 3, result: null, help: [2, 3, 5] },
@@ -36,7 +36,7 @@ const tasksList = [
 		],
 	},
 	{
-		type: "Вычитание",
+		type: 'Вычитание',
 		variants: [
 			{ firstNum: 4, secondNum: null, result: 3, help: [4, 1, 3] },
 			{ firstNum: 6, secondNum: 2, result: null, help: [6, 2, 4] },
@@ -46,18 +46,18 @@ const tasksList = [
 		],
 	},
 	{
-		type: "Порядковый счёт",
+		type: 'Порядковый счёт',
 		variants: [],
 	},
 	{
-		type: "Состав числа",
+		type: 'Состав числа',
 		variants: [],
 	},
 ];
 
 function Trainer() {
 	// Отображаемый компонент
-	const [activeComponent, setActiveComponent] = useState("TrainerInit");
+	const [activeComponent, setActiveComponent] = useState('TrainerInit');
 	// Объект с информацией о ребёнке
 	const [selectedChild, setSelectedChild] = useState(null);
 	// Выбранный тип задачи "Сложение" = 0/"Вычитание" = 1/"Порядковый счёт" = 2/"Состав числа" = 3
@@ -73,7 +73,7 @@ function Trainer() {
 	let displayedComponent;
 	switch (activeComponent) {
 		// Выбор ребёнка для прохождения тренажёра
-		case "TrainerInit":
+		case 'TrainerInit':
 			displayedComponent = (
 				<TrainerInit
 					setActiveComponent={setActiveComponent}
@@ -83,7 +83,7 @@ function Trainer() {
 			);
 			break;
 		// Выбор необходимого типа задачи
-		case "TrainerTask":
+		case 'TrainerTask':
 			displayedComponent = (
 				<TrainerTask
 					setActiveComponent={setActiveComponent}
@@ -91,8 +91,8 @@ function Trainer() {
 				/>
 			);
 			break;
-		// Выбор стимулов (карточке)
-		case "CardSelection":
+		// Выбор стимулов (карточек)
+		case 'CardSelection':
 			displayedComponent = (
 				<CardSelection
 					cardsImgs={cardsImgs}
@@ -103,9 +103,15 @@ function Trainer() {
 			);
 			break;
 		// Задачи
-		case "Task":
+		case 'Task':
 			displayedComponent = (
 				<Task
+					// функция, которая вызывается с аргументом типа задания: getExpressions(0 / 1), 0 - сложение, 1 - вычитание
+					// на каждой итерации =>
+					// 	случайно выбирать неизвестное число = null: firstNumber, secondNumber, result
+					// 	генерировать остальные два числа случайным образом
+					// 	генерировать банк подсказки
+					// 	возвращать выражение в виде объекта
 					mathExpressions={tasksList[selectedTypeTask]}
 					iteration={iteration}
 					setIteration={setIteration}
@@ -114,13 +120,13 @@ function Trainer() {
 			);
 			break;
 		// Таймер-отвлечение после ошибки (хлопаем в ладоши)
-		case "Timer":
+		case 'Distraction':
 			displayedComponent = (
-				<HandClapTimer timeCount={30} setActiveComponent={setActiveComponent} />
+				<Distraction setActiveComponent={setActiveComponent} />
 			);
 			break;
 		// Награждение карточкой за успешно решённую задачу
-		case "CardGreeting":
+		case 'CardGreeting':
 			displayedComponent = (
 				<CardGreeting
 					name={selectedChild.firstName}
@@ -131,14 +137,13 @@ function Trainer() {
 			);
 			break;
 		// Показ мультика после цикла задач
-		case "Cartoon":
+		case 'Cartoon':
 			const cards = selectedCards.map((number) => cardsImgs[number], []);
 			displayedComponent = <Cartoon cards={cards} />;
 			break;
-
 		default:
 			displayedComponent = (
-				<div className="">
+				<div className=''>
 					Взаимодействие недоступно. Произошла непредвиденная ошибка...
 				</div>
 			);
