279f2335e5c1953ee327e6a0a892ac6a464f7993
Anton Polyakov <a.polyakov@devgamemobile.com>
2026-01-22 10:11:07 +0700

    changed text, cartoon src

--- src/components/cartoonArray.js
+++ src/components/cartoonArray.js
@@ -1,7 +1,10 @@
 export const cartoonBankSrc = [
-	'https://rutube.ru/play/embed/a8f116f513794adf3191dd04bb96f3d4/',
-	'https://rutube.ru/play/embed/1cd59bb6ad5740e4d6f55e226496b8b8/',
-	'https://rutube.ru/play/embed/42c7195976a110cb80d0a6cd42398df3/',
-	'https://rutube.ru/play/embed/771da433749fc53c3f7937b7e218c026/',
-	'https://rutube.ru/play/embed/55193147ed720e0e57d267990d34dee2/',
+  // Недоступные мультики из-за АП
+  // 'https://rutube.ru/play/embed/a8f116f513794adf3191dd04bb96f3d4/',
+  // 'https://rutube.ru/play/embed/1cd59bb6ad5740e4d6f55e226496b8b8/',
+  // 'https://rutube.ru/play/embed/42c7195976a110cb80d0a6cd42398df3/',
+  // 'https://rutube.ru/play/embed/771da433749fc53c3f7937b7e218c026/',
+  "https://rutube.ru/play/embed/55193147ed720e0e57d267990d34dee2/",
+  "https://rutube.ru/play/embed/d26808e56b5ab05c8eb44564fe0d2360/",
+  "https://rutube.ru/play/embed/5e8d9d173690ea9af2375f92e8d0aac3/",
 ];
